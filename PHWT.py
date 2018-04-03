84b76e36be1764374b9c31f5e14d3193bc9be369
CBock879 <37879356+CBock879@users.noreply.github.com>
2018-04-03 10:50:21 -0400

    Algorithim Works
    This time for real real

--- PHWT.py
+++ PHWT.py
@@ -31,34 +31,29 @@ cst_trbn= 1100000                   #Cost of turbine
 cst_no_compressor  = 400000        #Cost of compressor 
 cst_reheater =  1   
 
-##finds the most cost effiecent regenerator cost per % 
-#for i in effects:
-#    effects[2][i] = effects[0][i] / effects[1][i]
-##sorts
-#effects.sort(key=lambda x:x[2])
-
-
+ 
 #no_comp: number of no_compressors
 #no_turb: number of no_turbines
 #rgn_ep regenerator epsilon ( effectivnes)
 
-t1 = 27.0 + 273.13                #t1 = temperature 1
-t3 = 1150.0 + 273.13            #t3: temperature 3
-Pr = 9.0                        #Pr = pressure ratio
-P1 = 100.0
-def power_plant(no_comp, no_turb, rgn_ep):
+#t1 = 27.0 + 273.13                #t1 = temperature 1
+#t3 = 1150.0 + 273.13            #t3: temperature 3
+#Pr = 9.0                        #Pr = pressure ratio
+#P1 = 100.0
+def power_plant(no_comp, no_turb, rgn_ep,t1,t3,P1,Pr):
     
     #fluid = IAPWS97(T = 0, P = 2)
     P = []
     T = []
     win = 0 
     
+    
     # runs through each of the stages of the no_compression
     Prs  = Pr ** ( 1.0/ no_comp)    #Prs: pressure ratio for individual stages
     
                  
     #first compression stage
-    t2 = (Prs)**((k-1.0)/k) *t1       #t2: tempature 2
+    t2 = (Prs)**((k-1.0)/k) * t1       #t2: tempature 2
     win = (t2-t1)*Cp
     T.append(t1)
     T.append(t2)
@@ -70,19 +65,26 @@ def power_plant(no_comp, no_turb, rgn_ep):
     #subseqent compression stages
     if (no_comp > 1):
         for i in range(1,no_comp):
-            win += (t2-t1)*Cp
-            T.append(t1)
+            
+            T.append(t1)     
+            #t2 = (Prs)**((k-1.0)/k) * t2
             T.append(t2)
+            
+            win += (t2-t1)*Cp
+                  
+            
+            
             P.append(p)
             p = p*Prs
             P.append(p)
             
-     
+            
+    print(win)
     # find qin from inital heat addition
     qin = (t3 - t2) * Cp            #qin: heat input 
 
     #recalculates stage pressure ratio
-    Prs  = Pr ** ( 1.0/ no_turb)    #Prs: pressure ratio for individual stages  
+    Prs  = Pr ** (1.0/ no_turb)    #Prs: pressure ratio for individual stages  
     
                  
     #inital turbine stage
@@ -98,32 +100,36 @@ def power_plant(no_comp, no_turb, rgn_ep):
     #subsequent turbine stages
     if( no_turb > 1):
         for i in range(1,no_turb):
-            wout += (t3-t4) * Cp 
             qin += wout
             T.append(t3)
-            T.append(t4)
+            #t4 = t4 * (1/Prs)**((k-1)/k)   
+            wout += (t3-t4) * Cp 
+            T.append(t4)        
+            
+            
             P.append(p)
             p = p/Prs
             P.append(p)
 
+    print(wout)
     
     #find heat regained from regenerator
     q_regenable = (t4-t2)*Cp        # q_regnenable: heat able to be regenerated
     q_regen = q_regenable*rgn_ep    # q_regen: heat regenerated
-    
-    print((t3-t2)*Cp)
+     
+   # print((t3-t2)*Cp)
     #accounts for heat regenerated 
     qin = qin - q_regen
     #find net work 
     nwout = wout - win 
     
     
-    print(nwout)
-#    print(win)
-    print(qin)
+    #print(nwout)
+#   print(win)
+    #print(qin)
     output = (nwout/qin)
     pyp.plot(T,P)
-    print()
+    #print()
     return output
     
 def total_cost(no_comp, no_turb, regencost):
@@ -142,33 +148,24 @@ def life_cost(eta_th):
     return fuel_cost
 
 
-max = 9
+max = 4
 levels =  [0,1,2,3,4,5]
 #print(power_plant(1.0,1.0,0.81))
 
-effs = np.zeros([max,max])
-costs = np.zeros([max,max])
+#effs = np.zeros([max,max])
+#costs = np.zeros([max,max])
+#for i in range(0,max):
+#    for j in range(0,max):
+#        effs[i,j] = power_plant(i+1,j+1,0.6)
+#        costs[i,j] = total_cost(i+1,j+1,5000.0) + life_cost(effs[i,j])
+#     
+
 for i in range(1,max):
-    for j in range(1,max):
-        effs[i,j] = power_plant(i,j,0.6)
-        costs[i,j] = total_cost(i,j,5000.0) + life_cost(effs[i,j])
+    print(power_plant(i,i,0.7,300,1400,100,9))
 
-lin_eff  = np.zeros([max])      
-
-#for i in range(1,max):
-#    lin_eff[i] = power_plant(i,i,0.8)
-#      
-print(lin_eff)
 
 #p1 = pyp.figure(1)
-#pyp.contourf(costs, 100, vmin=0)
-#p2 = pyp.figure(2)
 #pyp.contourf(effs, 100)
-#p3 = pyp.figure(3)
-#pyp.plot(lin_eff)
-#p1.show()
-#p2.show()
-#p3.show()
 
         
         
