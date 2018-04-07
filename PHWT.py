98c443085864557363e618684ba3eed7bf306cc2
CBock879 <37879356+CBock879@users.noreply.github.com>
2018-04-07 15:17:26 -0400

    Created PPTools
    Moved larger functions to seperate file

--- PHWT.py
+++ PHWT.py
@@ -9,6 +9,7 @@ Created on Sun Mar 25 12:10:55 2018
 import numpy as np
 import matplotlib.pyplot as pyp
 import sympy 
+import PPTools
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -19,96 +20,7 @@ regen_epsilon = [0.5,0.7,0.8,0.9,0.96,0.98]
 regen_costs =   [2200000,2500000,3100000,4000000,5200000,6700000]
 
  
-#no_comp: number of no_compressors
-#no_turb: number of no_turbines
-#rgn_ep regenerator epsilon ( effectivnes)
 
-def power_plant(no_comp, no_turb, rgn_ep,t1,t3,P1,Pr):
-    
-    
-    Cp = 1.004                          #Specific heat                    
-    k = 1.4 
-    P = []
-    T = []
-    win = 0 
-
-    
-    # runs through each of the stages of the no_compression
-    Prs  = Pr ** ( 1.0/ no_comp)    #Prs: pressure ratio for individual stages
-                 
-    #first compression stage
-    t2 = (Prs)**((k-1.0)/k) * t1       #t2: tempature 2
-    win = (t2-t1)*Cp
-    T.append(t1)
-    T.append(t2)
-    P.append(P1)
-    p = P1*Prs
-    P.append(p)
-    
-    
-    #subseqent compression stages
-    if (no_comp > 1):
-        for i in range(1,no_comp):
-            
-            T.append(t1)     
-            #t2 = (Prs)**((k-1.0)/k) * t2
-            T.append(t2)
-            
-            win += (t2-t1)*Cp
-                  
-            
-            
-            P.append(p)
-            p = p*Prs
-            P.append(p)
-            
-            
-    # find qin from inital heat addition
-    qin = (t3 - t2) * Cp            #qin: heat input 
-
-    #recalculates stage pressure ratio
-    Prs  = Pr ** (1.0/ no_turb)    #Prs: pressure ratio for individual stages  
-    
-                 
-    #inital turbine stage
-    t4 = t3 * (1/Prs)**((k-1)/k)    #t4: temperature 4
-    wout = (t3-t4) * Cp 
-    T.append(t3)
-    T.append(t4)
-    P.append(p)
-    p = p/Prs
-    P.append(p)
-    
-    
-    #subsequent turbine stages
-    if( no_turb > 1):
-        for i in range(1,no_turb):
-            qin += wout
-            T.append(t3)
-            #t4 = t4 * (1/Prs)**((k-1)/k)   
-            wout += (t3-t4) * Cp 
-            T.append(t4)        
-            
-            
-            P.append(p)
-            p = p/Prs
-            P.append(p)
-            
-    #find heat regained from regenerator
-    q_regenable = (t4-t2)*Cp        # q_regnenable: heat able to be regenerated
-    q_regen = q_regenable*rgn_ep    # q_regen: heat regenerated
-     
-    #accounts for heat regenerated 
-    qin = qin - q_regen
-    
-    #find net work 
-    nwout = wout - win 
-    
-    #pyp.plot(T,P)
-    
-    output = float(nwout/qin)
-    return output
-    
 
 
 def total_cost(no_comp, no_turb, regencost):
@@ -148,24 +60,27 @@ pyp.legend('1234')
 pyp.show()
 
 
-#max = 5
-#effs = np.zeros([max,max,6],dtype=float)
-#costs = np.zeros([max,max,6],dtype = float )
-#cst_min = 9000000000000000.0
-#best_config = np.zeros([3])
-#for i in range(0,max):
-#    for j in range(0,max):
-#        for k in range(0,6):
-#            
-#            #finds data for this configuration
-#            eff = power_plant(i+1,j+1,regen_epsilon[k],300,1400,100,9) 
-#            effs[i,j,k] = eff
-#            costs[i,j,k] = total_cost(i+1,j+1,regen_costs[k]) + life_cost(eff)
-#            #finds if this configuration is better
-#            if (cst_min> costs[i,j,k]):
-#                cst_min = costs[i,j,k]
-#                best_config=[i,j,k]
-#                print(i+1,"compressors",j+1,"turbines",regen_costs[k])
+#part B
+max = 5
+effs = np.zeros([max,max,6],dtype=float)
+costs = np.zeros([max,max,6],dtype = float )
+cst_min = 9000000000000000.0
+best_config = np.zeros([3]) 
+for i in range(0,max):
+    for j in range(0,max):
+        for k in range(0,6):
+            
+            #finds data for this configuration
+            eff = power_plant(i+1,j+1,regen_epsilon[k],300,1400,100,9) 
+            effs[i,j,k] = eff
+            costs[i,j,k] = total_cost(i+1,j+1,regen_costs[k]) + life_cost(eff)
+            #finds if this configuration is better
+            if (cst_min> costs[i,j,k]):
+                cst_min = costs[i,j,k]
+                best_config=[i,j,k]
+                print(i+1,"compressors",j+1,"turbines",regen_costs[k])
+
+
 
 
 #temp_range = np.linspace(250,500,100)
---
+++ PPTools.py
@@ -0,0 +1,268 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  7 15:11:40 2018
+
+@author: Chris
+"""
+
+#no_comp: number of no_compressors
+#no_turb: number of no_turbines
+#rgn_ep regenerator epsilon ( effectivnes)
+def power_plant(no_comp, no_turb, rgn_ep,t1,t3,P1,Pr):
+    
+    
+    Cp = 1.004                          #Specific heat                    
+    k = 1.4 
+    P = []
+    T = []
+    win = 0 
+
+    
+    # runs through each of the stages of the no_compression
+    Prs  = Pr ** ( 1.0/ no_comp)    #Prs: pressure ratio for individual stages
+                 
+    #first compression stage
+    t2 = (Prs)**((k-1.0)/k) * t1       #t2: tempature 2
+    win = (t2-t1)*Cp
+    T.append(t1)
+    T.append(t2)
+    P.append(P1)
+    p = P1*Prs
+    P.append(p)
+    
+    
+    #subseqent compression stages
+    if (no_comp > 1):
+        for i in range(1,no_comp):
+            
+            T.append(t1)     
+            #t2 = (Prs)**((k-1.0)/k) * t2
+            T.append(t2)
+            
+            win += (t2-t1)*Cp
+                  
+            
+            
+            P.append(p)
+            p = p*Prs
+            P.append(p)
+            
+            
+    # find qin from inital heat addition
+    qin = (t3 - t2) * Cp            #qin: heat input 
+
+    #recalculates stage pressure ratio
+    Prs  = Pr ** (1.0/ no_turb)    #Prs: pressure ratio for individual stages  
+    
+                 
+    #inital turbine stage
+    t4 = t3 * (1/Prs)**((k-1)/k)    #t4: temperature 4
+    wout = (t3-t4) * Cp 
+    T.append(t3)
+    T.append(t4)
+    P.append(p)
+    p = p/Prs
+    P.append(p)
+    
+    
+    #subsequent turbine stages
+    if( no_turb > 1):
+        for i in range(1,no_turb):
+            qin += wout
+            T.append(t3)
+            #t4 = t4 * (1/Prs)**((k-1)/k)   
+            wout += (t3-t4) * Cp 
+            T.append(t4)        
+            
+            
+            P.append(p)
+            p = p/Prs
+            P.append(p)
+            
+    #find heat regained from regenerator
+    q_regenable = (t4-t2)*Cp        # q_regnenable: heat able to be regenerated
+    q_regen = q_regenable*rgn_ep    # q_regen: heat regenerated
+     
+    #accounts for heat regenerated 
+    qin = qin - q_regen
+    
+    #find net work 
+    nwout = wout - win 
+    
+    #pyp.plot(T,P)
+    
+    output = float(nwout/qin)
+    return output
+
+#no_comp: number of no_compressors
+#no_turb: number of no_turbines
+#rgn_ep regenerator epsilon ( effectivnes)
+
+def power_plant_T(no_comp, no_turb, rgn_ep,t1,t3,P1,Pr):
+    
+    
+    Cp = 1.004                          #Specific heat                    
+    k = 1.4 
+    P = []
+    T = []
+    win = 0 
+
+    
+    # runs through each of the stages of the no_compression
+    Prs  = Pr ** ( 1.0/ no_comp)    #Prs: pressure ratio for individual stages
+                 
+    #first compression stage
+    t2 = (Prs)**((k-1.0)/k) * t1       #t2: tempature 2
+    win = (t2-t1)*Cp
+    T.append(t1)
+    T.append(t2)
+    P.append(P1)
+    p = P1*Prs
+    P.append(p)
+    
+    
+    #subseqent compression stages
+    if (no_comp > 1):
+        for i in range(1,no_comp):
+            
+            T.append(t1)     
+            #t2 = (Prs)**((k-1.0)/k) * t2
+            T.append(t2)
+            
+            win += (t2-t1)*Cp
+                  
+            
+            
+            P.append(p)
+            p = p*Prs
+            P.append(p)
+            
+            
+    # find qin from inital heat addition
+    qin = (t3 - t2) * Cp            #qin: heat input 
+
+    #recalculates stage pressure ratio
+    Prs  = Pr ** (1.0/ no_turb)    #Prs: pressure ratio for individual stages  
+    
+                 
+    #inital turbine stage
+    t4 = t3 * (1/Prs)**((k-1)/k)    #t4: temperature 4
+    wout = (t3-t4) * Cp 
+    T.append(t3)
+    T.append(t4)
+    P.append(p)
+    p = p/Prs
+    P.append(p)
+    
+    
+    #subsequent turbine stages
+    if( no_turb > 1):
+        for i in range(1,no_turb):
+            qin += wout
+            T.append(t3)
+            #t4 = t4 * (1/Prs)**((k-1)/k)   
+            wout += (t3-t4) * Cp 
+            T.append(t4)        
+            
+            
+            P.append(p)
+            p = p/Prs
+            P.append(p)
+            
+    #find heat regained from regenerator
+    q_regenable = (t4-t2)*Cp        # q_regnenable: heat able to be regenerated
+    q_regen = q_regenable*rgn_ep    # q_regen: heat regenerated
+     
+    #accounts for heat regenerated 
+    qin = qin - q_regen
+    
+    #find net work 
+    nwout = wout - win 
+    
+    #pyp.plot(T,P)
+    return T
+
+def power_plant_P(no_comp, no_turb, rgn_ep,t1,t3,P1,Pr):
+    
+    
+    Cp = 1.004                          #Specific heat                    
+    k = 1.4 
+    P = []
+    T = []
+    win = 0 
+
+    
+    # runs through each of the stages of the no_compression
+    Prs  = Pr ** ( 1.0/ no_comp)    #Prs: pressure ratio for individual stages
+                 
+    #first compression stage
+    t2 = (Prs)**((k-1.0)/k) * t1       #t2: tempature 2
+    win = (t2-t1)*Cp
+    T.append(t1)
+    T.append(t2)
+    P.append(P1)
+    p = P1*Prs
+    P.append(p)
+    
+    
+    #subseqent compression stages
+    if (no_comp > 1):
+        for i in range(1,no_comp):
+            
+            T.append(t1)     
+            #t2 = (Prs)**((k-1.0)/k) * t2
+            T.append(t2)
+            
+            win += (t2-t1)*Cp
+                  
+            
+            
+            P.append(p)
+            p = p*Prs
+            P.append(p)
+            
+            
+    # find qin from inital heat addition
+    qin = (t3 - t2) * Cp            #qin: heat input 
+
+    #recalculates stage pressure ratio
+    Prs  = Pr ** (1.0/ no_turb)    #Prs: pressure ratio for individual stages  
+    
+                 
+    #inital turbine stage
+    t4 = t3 * (1/Prs)**((k-1)/k)    #t4: temperature 4
+    wout = (t3-t4) * Cp 
+    T.append(t3)
+    T.append(t4)
+    P.append(p)
+    p = p/Prs
+    P.append(p)
+    
+    
+    #subsequent turbine stages
+    if( no_turb > 1):
+        for i in range(1,no_turb):
+            qin += wout
+            T.append(t3)
+            #t4 = t4 * (1/Prs)**((k-1)/k)   
+            wout += (t3-t4) * Cp 
+            T.append(t4)        
+            
+            
+            P.append(p)
+            p = p/Prs
+            P.append(p)
+            
+    #find heat regained from regenerator
+    q_regenable = (t4-t2)*Cp        # q_regnenable: heat able to be regenerated
+    q_regen = q_regenable*rgn_ep    # q_regen: heat regenerated
+     
+    #accounts for heat regenerated 
+    qin = qin - q_regen
+    
+    #find net work 
+    nwout = wout - win 
+    
+    #pyp.plot(T,P)
+    
+    return P
