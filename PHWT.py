a2a31f8d16db5fdcb655f4b38b43d0eea0b9d00f
CBock879 <37879356+CBock879@users.noreply.github.com>
2018-04-08 17:20:54 -0400

    Part C
    added part C

--- PHWT.py
+++ PHWT.py
@@ -35,7 +35,7 @@ def total_cost(no_comp, no_turb, regencost):
 def life_cost(eta_th):
     
     #total amount of energy (J) power plant puts out in life
-    total_life_power =  4000000.0*60.0*60.0*24.0*365.0*20.0
+    total_life_power =  4000000.0*60.0*60.0*24.0*365.25*20.0
     
     #fuel properties
     fuel_cost_per_unit = 0.28
@@ -55,9 +55,8 @@ for i in range (1,5):
     epsilon_sweep = np.linspace(0,1,200)
     eff = PPT.power_plant(i,i,epsilon_sweep,300,K(1150),100,9)
     fuel_cost = life_cost(eff)
-    print(fuel_cost)
     
-    pyp.legend('4321')   
+      
     pyp.xlabel('regenerator effectiveness')
     pyp.subplot(211)
     pyp.xticks([])
@@ -70,12 +69,14 @@ for i in range (1,5):
     
     
 
-
+pyp.legend('1234') 
 fig1.show()
 
 #
 
 #part B
+
+print("Part B")
 max = 5
 cst_min = 9000000000000000.0 
 regen_cost = 0
@@ -107,31 +108,40 @@ best_config.outlet_states()
 print()
 
 #prints net work
-print('Net specific work of cycle',best_config.nwork)
+print('Net specific work of cycle',best_config.nwork, 'kJ/kg')
+print()
+
 
 #prints back work ratio
 print('Back work ratio' , best_config.back_work_ratio)
+print()
 
 
 #calculates air flow
 mass_flow = best_config.air_flow(4000000)
 print("Air flow of best configuration",mass_flow, 'kg/s')
+print()
 
 #heat input
 print('rate of heat input', best_config.nq, 'kJ/kg')
+print()
 
 #calculated fuel consumption
 mass_flow = best_config.fuel_flow(heat_per_unit,4000000)
-print("Fuel flow of best configuration",mass_flow)
+print("Fuel flow of best configuration",mass_flow, 'kg/s')
+print()
 
 #gets total lifetime fuel cost
-print('total fuel cost over 20 years',life_cost(best_config.eta))
+print('total fuel cost over 20 years  $',life_cost(best_config.eta), )
+print()
 
 #gets total equipment cost
-print('total equipment cost', best_config.equipement_cost(regen_cost))
+print('total equipment cost  $', best_config.equipement_cost(regen_cost))
+print()
 
 #gets cost of best configuration
 print('Total cost of best configuration: $', (cst_min))
+print()
 
 comps  = best_config.compressors
 turbs  = best_config.turbines
@@ -147,26 +157,90 @@ pyp.xlabel('Temperature (deg C)')
 pyp.ylabel('$\eta_{th}$')
 fig2.show()
 
-
-#max = 5
-#effs = np.zeros([max,max,6],dtype=float)
-#costs = np.zeros([max,max,6],dtype = float )
-#cst_min = 9000000000000000.0
-#best_config = np.zeros([3]) 
-#for i in range(0,max):
-#        for k in range(0,6):
-#            
-#            #finds data for this configuration
-#            eff = PPT.power_plant(i+1,j+1,regen_epsilon[k],300,K(1150),100,9) 
-#            effs[i,j,k] = eff
-#            costs[i,j,k] = total_cost(i+1,j+1,regen_costs[k]) + life_cost(eff)
-#            #finds if this configuration is better
-#            if (cst_min> costs[i,j,k]):
-#                cst_min = costs[i,j,k]
-#                best_config=[i+1,j+1,k]
-#                print(i+1,"compressors",j+1,"turbines",regen_costs[k])
+print()
+print()
+print()
 
 
-#pyp.ylabel(r'$\eta_{th}$')
-#pyp.xlabel("Outside Temperature (K)")
-#pyp.plot(temp_range,temp_sweep)     
+#Part C
+
+print("PartC")
+best_config = PPT.PPlant(1,1,0,300,1000,100,9)
+cst_min = 9000000000000000.0
+
+
+for i in range(0,max):
+    for j in range(0,max):
+        for k in range(0,6):
+            #finds data for this configuration
+            eff = PPT.power_plant(i+1,j+1,regen_epsilon[k],300,K(1150),100,9) 
+            cost = total_cost(i+1,j+1,regen_costs[k]) + life_cost(eff)
+            #finds if this configuration is better
+            if (cst_min> cost):
+                best_config = PPT.PPlant(i+1,j+1,regen_epsilon[k],300,K(1150),100,9)
+                cst_min = cost
+                regen_cost = regen_costs[k]
+                
+#gets efficency
+print('Thermal efficency of best configuration', best_config.eta)
+         
+print()
+
+print('best config', best_config.compressors, "compressors",
+      best_config.turbines, "turbines")
+
+#prints outlet states       
+best_config.outlet_states()
+
+print()
+
+#prints net work
+print('Net specific work of cycle',best_config.nwork, 'kJ/kg')
+
+print()
+
+#prints back work ratio
+print('Back work ratio' , best_config.back_work_ratio)
+
+print()
+
+#calculates air flow
+mass_flow = best_config.air_flow(4000000)
+print("Air flow of best configuration",mass_flow, 'kg/s')
+print()
+
+#heat input
+print('rate of heat input', best_config.nq, 'kJ/kg')
+print()
+
+#calculated fuel consumption
+mass_flow = best_config.fuel_flow(heat_per_unit,4000000)
+print("Fuel flow of best configuration",mass_flow, 'kg/s')
+
+print()
+#gets total lifetime fuel cost
+print('total fuel cost over 20 years  $',life_cost(best_config.eta), )
+print()
+
+#gets total equipment cost
+print('total equipment cost  $', best_config.equipement_cost(regen_cost))
+print()
+
+#gets cost of best configuration
+print('Total cost of best configuration: $', (cst_min))
+print()
+
+comps  = best_config.compressors 
+turbs  = best_config.turbines
+
+temp_range = np.linspace(-30,40,100)
+epsilon = best_config.epsilon
+eta_range = PPT.power_plant(comps,turbs,epsilon,K(temp_range),K(1150),100,9)
+
+fig3 = pyp.figure(2)
+pyp.plot(temp_range,eta_range)
+pyp.title('Tempature vs $\eta_{th}$')
+pyp.xlabel('Temperature (deg C)')
+pyp.ylabel('$\eta_{th}$')
+fig3.show()
+
